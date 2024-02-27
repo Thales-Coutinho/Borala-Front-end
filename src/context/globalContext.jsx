bdec5d2170e6ec9468221a99e2b5632210deddae
Thales Coutinho <thales9696@protonmail.com>
2024-02-27 19:51:57 -0300

    api tweaks

--- src/context/globalContext.jsx
+++ src/context/globalContext.jsx
@@ -13,7 +13,7 @@ export const GlobbalProvider = ({ children }) => {
     useEffect(() => {
         apiRest.get('cities/')
             .then(response => {
-                setCities(response.data.cities)
+                setCities(response.data)
             })
             .catch(error => 
                 alert("erro ao carregar Cidades error:" + error)
@@ -25,7 +25,7 @@ export const GlobbalProvider = ({ children }) => {
             params: filters,
         })
             .then(response => {
-                setTrips(response.data.trips)
+                setTrips(response.data)
             })
             .catch(error => 
                 alert("erro ao carregar viagens error:" + error)
