72e3d07b73a347a61e012bcbbf5164185517e60a
Thales Martim Coutinho <thales9696@protonmail.com>
2024-03-10 16:07:24 -0300

    axios headers

--- src/utils/apiRest.js
+++ src/utils/apiRest.js
@@ -1,7 +1,11 @@
 import axios from "axios"
 
 const apiRest = axios.create({
-    baseURL: 'http://localhost:8080/'
+    baseURL: 'http://localhost:8080/',
+    headers: {
+        Accept: 'application/json',
+        Content: 'application/json'
+    }
 })
 
 export default apiRest
